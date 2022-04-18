89eb61267ec0c4f7f825f2109da39ce838ec4322
Matias Björklund <matias_bjorklund@hotmail.com>
2022-04-18 19:54:08 +0300

    step2 done

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,3 +1,44 @@
+import { Component, useReducer } from "react"
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.headertext}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
+
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} exercise={props.exercises[0]}/>
+      <Part part={props.parts[1]} exercise={props.exercises[1]}/>
+      <Part part={props.parts[2]} exercise={props.exercises[2]}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let sum = 0
+  for (let i = 0; i < props.excersises.length; i++) {
+    sum = props.excersises[i];
+    
+  }
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -7,19 +48,13 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const parts = [part1,part2,part3]
+  const exercises = [exercises1,exercises2,exercises3]
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header headertext={course} />
+      <Content  parts={parts} exercises={exercises}/>
+      <Total exercises={exercises}/>
     </div>
   )
 }
